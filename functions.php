d65dd296a842248dd5662432e13b785a5d6d2753
Donald Louch <donaldlouch@outlook.com>
2018-07-23 16:40:44 -0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+/*
+=====================================================================
+	Calls the parent theme's stylesheet
+=====================================================================
+*/
+  add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+  function enqueue_parent_styles() {
+      wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+  }
+
+/*
+=====================================================================
+	Update Checker
+		- Plugin Update Checker By YahnisElsts
+=====================================================================
+*/
+
+  		require 'plugin-update-checker/plugin-update-checker.php';
+  		$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  			'https://github.com/DonaldLouch/modernSherbetChild',
+  			__FILE__,
+  			'modernsherbet-child'
+  		);
+  		$myUpdateChecker->setBranch('stable');
+
+/*
+=====================================================================
+	Your Custom Code!
+=====================================================================
+*/
+
+  // Insert your custom functions here!
+
+?>
